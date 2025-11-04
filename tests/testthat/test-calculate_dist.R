9c2263d4b52938ccf8ee4385086efb2f2b76a2db
Laeticia Rachel Liawas <laeticia.rachel.liawas@gmail.com>
2025-11-04 01:16:42 -0500

    add tests for distance fxs

--- tests/testthat/test-calculate_dist.R
+++ tests/testthat/test-calculate_dist.R
@@ -1,6 +1,43 @@
 library(clustsimlR)
 
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("raise error if matrices are not square", {
+    matA <- matrix(c(1, 2, 3, 4, 5, 6), nrow = 3, ncol = 2)
+    testthat::expect_error(calculate_dist(matA, matA),
+            "Matrices must both be square, and have the same dimensions. \n")
 })
+
+test_that("raise error if matrices have different dimensions", {
+    matA <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+    matB <- matrix(c(1, 2, 3, 4, 5, 6, 7, 8, 9), nrow = 3, ncol = 3)
+
+    testthat::expect_error(calculate_dist(matA, matB),
+            "Matrices must both be square, and have the same dimensions. \n")
+})
+
+test_that("raise error if matrices are not positive", {
+    matA <- matrix(c(1, -2, 3, 4), nrow = 2, ncol = 2)
+    matB <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+
+    testthat::expect_error(calculate_dist(matA, matB),
+                           "Matrices must be positive. \n")
+    testthat::expect_error(calculate_dist(matB, matA),
+                           "Matrices must be positive. \n")
+    testthat::expect_error(calculate_dist(matA, matA),
+                           "Matrices must be positive. \n")
+})
+
+test_that("raise error if matrices have complex numbers", {
+    matA <- matrix(c(1, 2i, 3, 4), nrow = 2, ncol = 2)
+    matB <- matrix(c(1, 2, 3, 4), nrow = 2, ncol = 2)
+
+    testthat::expect_error(calculate_dist(matA, matB),
+                           "Matrices must only have real numbers. \n")
+    testthat::expect_error(calculate_dist(matB, matA),
+                           "Matrices must only have real numbers. \n")
+    testthat::expect_error(calculate_dist(matA, matA),
+                           "Matrices must only have real numbers. \n")
+})
+
+
+#TODO: add more tests?
--- tests/testthat/test-intercluster_dist.R
+++ tests/testthat/test-intercluster_dist.R
@@ -1,3 +1,17 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(clustsimlR)
+
+test_that("distance matrix is symmetric", {
+    ok_df <- data.frame(matrix(runif(400), nrow = 100, ncol = 4))
+    gmm <- fit_GMM(ok_df)
+    dist_mat <- intercluster_dist(gmm, plot = FALSE)
+    testthat::expect_true(isSymmetric(dist_mat))
 })
+
+test_that("diagonal of distance matrix is 0", {
+    ok_df <- data.frame(matrix(runif(400), nrow = 100, ncol = 4))
+    gmm <- fit_GMM(ok_df)
+    dist_mat <- intercluster_dist(gmm, plot = FALSE)
+    testthat::expect_equal(sum(diag(dist_mat)), 0)
+})
+
+#TODO: add other tests
